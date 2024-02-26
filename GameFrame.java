4e3b6b2b4d088d03867ab60addb71cf21fb10f16
DipaliKarpe <161292095+DipaliKarpe@users.noreply.github.com>
2024-02-26 13:44:38 +0530

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,15 @@
+package Game;
+import javax.swing.JFrame;
+public class GameFrame extends JFrame {
+
+	GameFrame() {
+		GamePanel panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
